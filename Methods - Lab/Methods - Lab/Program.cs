5dcab22b89f25ec4de19faaf077b8eb2405e5d89
srpss <61586478+srpss@users.noreply.github.com>
2021-03-30 19:10:06 +0300

    Add files via upload

---
+++ Methods - Lab/Methods - Lab/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Methods___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*https://judge.softuni.bg/Contests/Practice/Index/1208#0
+             * 01. Sign of Integer Numbers
+             * 
+             
+
+            Program();
+
+             void Program() 
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    Console.WriteLine($"The number {number} is zero.");
+                }
+                else if (number>0)
+                {
+                    Console.WriteLine($"The number {number} is positive.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {number} is negative.");
+                }
+            }
+
+
+            02.Grades
+            
+
+
+            decimal input = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calc(input));
+            string Calc(decimal input)
+            {
+                string output = "";
+              
+
+                if (input >= 2 && input < 3)
+                {
+                    output = "Fail";
+                }
+                else if (input < 3.50m)
+                {
+                    output = "Poor";
+                }
+                else if (input < 4.50m)
+                {
+                    output = "Good";
+                }
+                else if (input < 5.50m)
+                {
+                    output = "Very good";
+                }
+                else if (input <= 6m)
+                {
+                    output = "Excellent";
+                }
+
+
+                return output;
+            }
+
+            03.Calculations
+
+         
+
+           
+            string input = (Console.ReadLine());
+
+            decimal input2 = decimal.Parse(Console.ReadLine());
+            decimal input3 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calc(input, input2, input3));
+
+            decimal Calc(string input, decimal input2, decimal input3)
+            {
+                decimal total = 0;
+                if (input == "add")
+                {
+                    total = input2 + input3;
+                }
+                else if (input == "multiply")
+                {
+                    total = input2 * input3;
+                }
+                else if (input == "subtract")
+                {
+                    total = input2 - input3;
+                }
+                else if (input == "divide")
+                {
+                    total = input2 / input3;
+                }
+                return total;
+            }
+            04.Printing Triangle
+               
+
+            int number = int.Parse(Console.ReadLine());
+            Pyramid(number);
+
+            void Pyramid(int number = 1)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j+ " ");
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = number - 1; i >= 0; i--)
+                {
+                    for (int j = 1; j  <= i; j++)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            05.Orders
+            too easy, skip to next
+            */
+
+
+        }
+    }
+}
